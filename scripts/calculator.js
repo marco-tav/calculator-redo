aa10d220e60834e1844262c12eed827de446b974
marco-tav <marco.tavella@outlook.com>
2023-10-26 21:30:44 -0300

    Add basic functions and correct indentation

---
+++ scripts/calculator.js
@@ -0,0 +1,20 @@
+/*------------------- Function Declarations --------------------*/ 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num2 === 0 ? "The calculator exploded" : num1 / num2;
+}
+
+
+
+/*---------------------- Main Program --------------------------*/
