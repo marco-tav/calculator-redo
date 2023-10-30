c26cbf6477ae933500dc56e8779df58eb8abab18
marco-tav <marco.tavella@outlook.com>
2023-10-30 11:23:40 -0300

    Add operate function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -15,6 +15,18 @@ function divide(num1, num2) {
   return num2 === 0 ? "The calculator exploded" : num1 / num2;
 }
 
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "x") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
+
 
 
 /*---------------------- Main Program --------------------------*/
